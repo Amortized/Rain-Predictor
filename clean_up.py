b5338e0c2dfeb523ad4772484fad57d23eccc4f7
Amortized <rahulmanghwani@gmail.com>
2015-05-08 01:55:04 -0700

    writing preprocessed output to file

--- clean_up.py
+++ clean_up.py
@@ -1,4 +1,5 @@
 import sys;
+from debian.debtags import output
 
 
 def identify_radar_end_points(readings, metric):
@@ -33,23 +34,34 @@ def identify_radar_end_points(readings, metric):
 
 
 
-def clean(myfile):
+def clean(inp_file, output_file, train):
   data = [];
-
-  with open(myfile, "r") as f:
+  
+  file_output = open(output_file, 'w');
+  
+  X	  = [];
+  Y	  = [];
+  
+  with open(inp_file, "r") as f:
 
     header = f.readline().strip().split(',')
 
-    X      = [];
-    Y      = [];
     count  = 0;
     for line in f:
+    	
     	if count % 10000 == 0:
     		print("Read " + str(count) + " lines");
+    	
     	count += 1;
     	
         x = line.strip().split(',');
+        
+        label_range = len(x);
+        if train:
+			label_range = len(x) - 1;
+			
 
+        
         radar_pos_time = identify_radar_end_points(x[1].split(), "time");
         radar_pos_dis  = identify_radar_end_points(x[2].split(), "distance");
 
@@ -58,30 +70,63 @@ def clean(myfile):
         data_points_Y  = [];
 
         for i in range(0, len(x[1].split())):
-        	data_points_X.append( [int(x[0])] ); #Append Gauge Id
+           data_points_X.append( [int(x[0])] ); #Append Gauge Id
 
-        for i in range(1, len(x) - 1):
+        for i in range(1, label_range):
            readings = x[i].split();
            for j in range(0,len(readings)):
            	  if readings[j] != 'nan':
            	  	 data_points_X[j].append(float(readings[j]));
            	  else:
            	     data_points_X[j].append("nan");
-
-
-        for j in range(0, len(x[1].split())):
-        	data_points_Y.append(float(x[len(x)-1]));
+           del readings;
+        
+        if train:
+           for j in range(0, len(x[1].split())):
+              data_points_Y.append(float(x[len(x)-1]));
         
     	X.extend(data_points_X);
     	Y.extend(data_points_Y);
-
-    assert(len(X) == len(Y))
-
+    	
+    	if len(X) > 50:
+    		for k in range(0, len(X)):
+    			if train:
+	    			for z in X[k]:
+	    			   file_output.write(str(z) + ",");
+		    		file_output.write(str(Y[k]));
+		    	else:
+		    		for z in range(0, len(X[k])):
+		    		   file_output.write(str(X[k][z]));
+		    		   if z < len(X[k]) - 1:
+		    		   	  file_output.write(",")
+		    		
+	    		file_output.write("\n");
+	    		
+	    	X = [];
+	    	Y = [];
+	    		 
+    
+    
+	
+  for k in range(0, len(X)):
+  	if train:
+  		for z in X[k]:
+  			file_output.write(str(z) + ",");
+  		file_output.write(str(Y[k]));
+	else:
+		for z in range(0, len(X[k])):
+			file_output.write(str(X[k][z]));
+			if z < len(X[k]) - 1:
+				file_output.write(",")
+	
+	file_output.write("\n");
+				
+  file_output.close();
 
 
 
 
 
 if __name__ == "__main__":
-   clean('./data/train_2013.csv');
-   clean('./data/test.csv');
+   clean('./data/train_2013.csv', './data/train_preprocessed.csv', True);
+   clean('./data/test_2014.csv', './data/test_preprocessed.csv', False);
